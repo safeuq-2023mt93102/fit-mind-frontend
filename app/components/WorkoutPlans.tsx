b8da98487ee426332faccc7695afac54a3bcf2bd
Nikhil <ndevare13@gmail.com>
2024-11-23 00:20:31 +0530

    updated API calls

--- app/components/WorkoutPlans.tsx
+++ app/components/WorkoutPlans.tsx
@@ -4,7 +4,8 @@ import {Button, Modal, Select, Table, Flex, Tag, Checkbox, Typography } from 'an
 import type { ColumnsType } from 'antd/es/table';
 import {useRouter} from "next/navigation";
 import {signOut} from "next-auth/react";
-import {Workout, Goal} from "@/interfaces/api/interfaces";
+import {Workout, Goal, Servers} from "@/interfaces/api/interfaces";
+import {callGet, callPost} from "@/util/util";
 
 const WorkoutPlans = () => {
   const router = useRouter();
@@ -12,7 +13,6 @@ const WorkoutPlans = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const [selectValue, setSelectValue] = useState("BEGINNER");
-  // const [workouts, setWorkouts] = useState<Workout[]>([]);
   const [goals, setGoals] = useState<Goal[]>([]);
   const [tableLoading, setTableLoading] = useState(true);
 
@@ -117,23 +117,20 @@ const WorkoutPlans = () => {
   const handleOk = () => {
     setLoading(true);
 
-    fetch("/api/post", {
-      method: "POST",
-      body: JSON.stringify({
-        path: '/plan',
-        payload: {
-          level: selectValue,
-          target: "GAIN_WEIGHT",
-        }
-      })
-    }).then((response) => {
+    callPost({
+      server: Servers.CORE,
+      path: '/plan',
+      payload: {
+        level: selectValue,
+        target: "GAIN_WEIGHT",
+      }
+    }).then(async (response) => {
       setLoading(false);
       switch (response.status) {
         case 200:
           setIsModalOpen(false);
           response.json().then((data: { workouts: Workout[] }) => {
             const workoutsList: Workout[] = data.workouts;
-            // setWorkouts(workoutsList);
             const goalIds = workoutsList.flatMap(workout => workout.goals);
             fetchGoals(goalIds)
           });
@@ -154,12 +151,11 @@ const WorkoutPlans = () => {
     setTableLoading(true);
     try {
       const goalDataPromises = goalIds.map(id =>
-        fetch("/api/get", {
-          method: "POST",
-          body: JSON.stringify({
-            path: '/goals/' + id
-          })
-        }).then((response) => {
+
+        callGet({
+          server: Servers.CORE,
+          path: '/goals/' + id
+        }).then(async (response) => {
           if (response.status === 401) {
             return signOut();
           }
@@ -178,22 +174,20 @@ const WorkoutPlans = () => {
   };
 
   const handleCheckboxChange = async (goal: Goal) => {
-    const updatedCompleted = !goal.completed;
+    const updatedCompleted: boolean = !goal.completed;
     
     const updatedGoals = goals.map((item) =>
       item.id === goal.id ? { ...item, completed: updatedCompleted } : item
     );
     setGoals(updatedGoals);
-  
-    fetch("/api/post", {
-      method: "POST",
-      body: JSON.stringify({
-        path: '/goals/' + goal.id,
-        payload: {
-          completed: updatedCompleted
-        }
-      })
-    }).then((response) => {
+
+    callPost({
+      server: Servers.CORE,
+      path: '/goals/' + goal.id,
+      payload: {
+        completed: updatedCompleted
+      }
+    }).then(async (response) => {
       if (response.status === 401) {
         return signOut();
       }
